14a10332a4a89eeeba1508187c13e26af1a34b61
Alexandre Mendes <alexandregme@gmail.com>
2018-05-23 17:18:36 -0300

    [app][hotsite][Form]

--- app/domains/hotsite/pages/landing/components/LandingPage/LandingPage.js
+++ app/domains/hotsite/pages/landing/components/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 import { BasicLayout } from 'core/components/BasicLayout'
+import { Form } from '../Form'
 
 export default class App extends Component {
   render () {
   return (
     <BasicLayout>
-      Hot Site Landing Page
+      Hot Site Landing Page'
+      <Form />
     </BasicLayout>
     );
   }
